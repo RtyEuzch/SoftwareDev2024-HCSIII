ff11e55e7bcbb2686a0c43ce93f09ca8fc4904f5
RtyEuzch <137963453+RtyEuzch@users.noreply.github.com>
2024-04-25 14:55:04 -0500

    Added code for the JComponent and JFrame

--- GameTester.java
+++ GameTester.java
@@ -1,5 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Double;
 public class GameTester {
-  public static void main(String[] args) {
-    
-  }
+    public static final int DIMENSION = 500;
+    public static void main(String[] args) {
+        JFrame introFrame = new JFrame("Intro Screen");
+        introFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        introFrame.setSize(DIMENSION, DIMENSION);
+        introFrame.setVisible(true);
+        introFrame.setResizable(false);
+        IntroScreen introComponent = new IntroScreen();
+        introFrame.add(introComponent);
+    }
+}
+
+class IntroScreen extends JComponent {
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.GREEN);
+        Rectangle2D.Double rec = new Rectangle2D.Double(0, 0, 39, 39);
+        g2.fill(rec);
+    }
 }
