42b79ed6150c408435a6b7d42bcdedc759305d98
RtyEuzch <137963453+RtyEuzch@users.noreply.github.com>
2024-05-01 18:40:52 -0500

    Update Player.java

---
+++ Player.java
@@ -0,0 +1,15 @@
+
+public class Player extends Tile {
+    public Player(int x, int y, int d) {
+        super(x, y, d, Tile.PLAYER_COLOR);
+    }
+
+    public void move(int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
+
+    @Override public Ellipse2D.Double getShape() {
+        return new Ellipse2D.Double(xCoor, yCoor, dimension, dimension);
+    }
+}
