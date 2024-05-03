916fe28b08eb32efa657bc5d1231974a17ac1d48
RtyEuzch <137963453+RtyEuzch@users.noreply.github.com>
2024-05-02 19:45:55 -0500

    Update TileEasy.java

--- TileEasy.java
+++ TileEasy.java
@@ -7,49 +7,49 @@
  * Collaborators: Charles Doan, Murtaza Khan
  */
 
-import java.awt.*;
-import java.awt.geom.*;
-
-public class TileEasy extends Tile
-{
-	//instance variables
-	private Rectangle2D.Double rect;
-	private boolean arrived;
-	
-	//constants
-	private static final Color VISITED = Color.YELLOW; 
-	
-	/** Creates the Easy Tile
-	 *  @param x the x coordinate of the tile
-	 *  @param y the y coordinate of the tile
-	 *  @param d the dimensions of the tile
-	 */
-	public TileEasy(int x, int y, int d)
-	{
-		super(x, y, d, PATH);
-		rect = new Rectangle2D.Double(getX(), getY(), getDimension(), getDimension());
-		arrived = false;
-	}
-	
-	/** Gets the rectangle made for the tile
-	 *  @return the rectangle of the tile
-	 */
-	public Rectangle2D.Double getRect()
-	{
-		return rect;
-	}
-	
-	/** Changes the color of the tile if it got visited
-	 */
-	public void isVisited()
-	{
-		this.setColor(VISITED);
-		arrived = true;
-	}
-
-	/** @return the bounding Shape of the Tile
+ import java.awt.*;
+ import java.awt.geom.*;
+ 
+ public class TileEasy extends Tile
+ {
+     //instance variables
+     private Rectangle2D.Double rect;
+     private boolean arrived;
+     
+     //constants
+     private static final Color VISITED = Color.YELLOW; 
+     
+     /** Creates the Easy Tile
+      *  @param x the x coordinate of the tile
+      *  @param y the y coordinate of the tile
+      *  @param d the dimensions of the tile
+      */
+     public TileEasy(int x, int y, int d, Color col)
+     {
+         super(x, y, d, col);
+         rect = new Rectangle2D.Double(getX(), getY(), getDimension(), getDimension());
+         arrived = false;
+     }
+     
+     /** Gets the rectangle made for the tile
+      *  @return the rectangle of the tile
+      */
+     public Rectangle2D.Double getRect()
+     {
+         return rect;
+     }
+     
+     /** Changes the color of the tile if it got visited
+      */
+     public void isVisited()
+     {
+         this.setColor(VISITED);
+         arrived = true;
+     }
+     
+    /** @return the bounding Shape of the Tile
 	 */
 	@Override public Rectangle2D.Double getShape() {
 		return new Rectangle2D.Double(xCoor, yCoor, dimension, dimension);
 	}
-}
+ }
