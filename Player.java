145cc5a7e691b1acaa0ed2c2c189122356ebed47
RtyEuzch <137963453+RtyEuzch@users.noreply.github.com>
2024-05-06 19:11:22 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,14 +1,32 @@
 import java.awt.geom.*;
 public class Player extends Tile {
+    /**
+     * Constructs a Player object using the x and y coordinates as well
+     * as the dimension of the bounding rectangle. The dimension is the
+     * length and width, since a Tile is a square.
+     * @param x the x coordinate
+     * @param y the y coordinate
+     * @param d the side length of the bounding rectangle of the square
+     */
     public Player(int x, int y, int d) {
         super(x, y, d, Tile.PLAYER_COLOR);
     }
 
+    /**
+     * Alters the position of the player internally; the 
+     * game screen still must be updated in order to reflect
+     * the changes.
+     * @param dx the change in the x coordiante
+     * @param dy the change in the y coordinate.
+     */
     public void move(int dx, int dy) {
         xCoor += dx;
         yCoor += dy;
     }
 
+    /**
+     * @return an Ellipse2D.Double that represents the player.
+     */
     @Override public Ellipse2D.Double getShape() {
         return new Ellipse2D.Double(xCoor, yCoor, dimension, dimension);
     }
